c6841336a19e18e64990e8db394a5e39b37b508a
Logan Barnes <barneslt63@gmail.com>
2017-04-11 01:42:11 +0100

    downloading glm automatically

--- src/graphics/Camera.cpp
+++ src/graphics/Camera.cpp
@@ -1,6 +1,8 @@
 #include "shared/graphics/Camera.hpp"
 
-#define GLFORCE_RADIANS_
+
+#define GLM_ENABLE_EXPERIMENTAL
+#define GLM_FORCE_RADIANS
 #include "glm/gtx/rotate_vector.hpp"
 #include "glm/gtx/vector_angle.hpp"
 #include <cmath>
