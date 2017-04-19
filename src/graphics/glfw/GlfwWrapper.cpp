cbeadc4192176ef320b30c9ea3828ca77f077138
Logan Barnes <barneslt63@gmail.com>
2017-04-19 16:12:58 -0700

    Attempting to perform virtual rendering on travis

--- src/graphics/glfw/GlfwWrapper.cpp
+++ src/graphics/glfw/GlfwWrapper.cpp
@@ -292,8 +292,8 @@ GlfwWrapper::_initGlfw( bool opengl )
   if ( opengl )
   {
     glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
-    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 2 );
-    glfwWindowHint( GLFW_OPENGL_PROFILE,        GLFW_OPENGL_CORE_PROFILE );
+    // glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 2 );
+    // glfwWindowHint( GLFW_OPENGL_PROFILE,        GLFW_OPENGL_CORE_PROFILE );
 #ifdef __APPLE__
     glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE );
 #endif
