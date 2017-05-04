ba85c98050deaa447cee720d52aeabc093eccae5
Logan Barnes <barneslt63@gmail.com>
2017-05-04 01:47:39 -0700

    Added standard pipeline helper function

--- include/shared/graphics/OpenGLHelper.hpp
+++ include/shared/graphics/OpenGLHelper.hpp
@@ -1,6 +1,8 @@
 // OpenGLHelper.hpp
 #pragma once
 
+#include "shared/graphics/GraphicsForwardDeclarations.hpp"
+
 #include <glad/glad.h>
 
 #include <string>
@@ -75,17 +77,17 @@ public:
   static
   void updateBuffer (
                      const std::shared_ptr< GLuint > &spBuffer,
-                     const size_t                    elementOffset,
-                     const size_t                    numElements,
-                     const T                        *pData,
-                     const GLenum                    bufferType
+                     const size_t                     elementOffset,
+                     const size_t                     numElements,
+                     const T                         *pData,
+                     const GLenum                     bufferType
                      );
 
   static
   std::shared_ptr< GLuint > createVao (
                                        const std::shared_ptr< GLuint > &spProgram,
                                        const std::shared_ptr< GLuint > &spVbo,
-                                       const GLsizei                   totalStride,
+                                       const GLsizei                    totalStride,
                                        const std::vector< VAOElement > &elements
                                        );
 
@@ -97,6 +99,19 @@ public:
                                                const std::shared_ptr< GLuint > spColorTex = nullptr,
                                                const std::shared_ptr< GLuint > spDepthTex = nullptr
                                                );
+
+  template< typename T >
+  static
+  StandardPipeline createStandardPipeline (
+                                           const std::string                vertFilePath,
+                                           const std::string                fragFilePath,
+                                           const T                         *pData,
+                                           const size_t                     numElements,
+                                           const GLsizei                    totalStride,
+                                           const std::vector< VAOElement > &elements,
+                                           const GLenum                     type = GL_ARRAY_BUFFER,
+                                           const GLenum                     usage = GL_STATIC_DRAW
+                                           );
 
   static
   void bindFramebuffer ( );
@@ -110,48 +125,48 @@ public:
   static
   void setTextureUniform (
                           const std::shared_ptr< GLuint > &spProgram,
-                          const std::string               uniform,
+                          const std::string                uniform,
                           const std::shared_ptr< GLuint > &spTexture,
-                          int                             activeTex
+                          int                              activeTex
                           );
 
   static
   void setIntUniform (
                       const std::shared_ptr< GLuint > &spProgram,
-                      const std::string               uniform,
-                      const int                      *pValue,
-                      const int                       size = 1,
-                      const int                       count = 1
+                      const std::string                uniform,
+                      const int                       *pValue,
+                      const int                        size = 1,
+                      const int                        count = 1
                       );
 
   static
   void setFloatUniform (
                         const std::shared_ptr< GLuint > &spProgram,
-                        const std::string               uniform,
-                        const float                    *pValue,
-                        const int                       size = 1,
-                        const int                       count = 1
+                        const std::string                uniform,
+                        const float                     *pValue,
+                        const int                        size = 1,
+                        const int                        count = 1
                         );
 
 
   static
   void setMatrixUniform (
                          const std::shared_ptr< GLuint > &spProgram,
-                         const std::string               uniform,
-                         const float                    *pValue,
-                         const int                       size = 4,
-                         const int                       count = 1
+                         const std::string                uniform,
+                         const float                     *pValue,
+                         const int                        size = 4,
+                         const int                        count = 1
                          );
 
   static
   void renderBuffer (
                      const std::shared_ptr< GLuint > &spVao,
-                     const int                       start,
-                     const int                       verts,
-                     const GLenum                    mode,
+                     const int                        start,
+                     const int                        verts,
+                     const GLenum                     mode,
                      const std::shared_ptr< GLuint > &spIbo = nullptr,
-                     const void                     *pOffset = 0,
-                     const GLenum                    iboType = GL_UNSIGNED_SHORT
+                     const void                      *pOffset = 0,
+                     const GLenum                     iboType = GL_UNSIGNED_SHORT
                      );
 
 
@@ -198,11 +213,11 @@ OpenGLHelper::createBuffer(
                            )
 {
   std::shared_ptr< GLuint > upBuffer( new GLuint,
-                                     [] ( auto pID )
-                                     {
-                                       glDeleteBuffers( 1, pID );
-                                       delete pID;
-                                     } );
+                                     [ ] ( auto pID )
+    {
+      glDeleteBuffers( 1, pID );
+      delete pID;
+    } );
 
   glGenBuffers( 1, upBuffer.get( ) );
   glBindBuffer( type, *upBuffer );
@@ -265,10 +280,10 @@ template< typename T >
 void
 OpenGLHelper::updateBuffer(
                            const std::shared_ptr< GLuint > &upBuffer,     ///<
-                           const size_t                    elementOffset, ///<
-                           const size_t                    numElements,   ///<
-                           const T                        *pData,         ///<
-                           const GLenum                    bufferType     ///<
+                           const size_t                     elementOffset, ///<
+                           const size_t                     numElements,  ///<
+                           const T                         *pData,        ///<
+                           const GLenum                     bufferType    ///<
                            )
 {
   constexpr auto typeSizeBytes = sizeof( T );
@@ -283,6 +298,51 @@ OpenGLHelper::updateBuffer(
 
   glBindBuffer( bufferType, 0 );
 } // OpenGLHelper::updateBuffer
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// \brief createStandardPipeline
+/// \return
+///
+/// \author Logan Barnes
+////////////////////////////////////////////////////////////////////////////////
+template< typename T >
+StandardPipeline
+OpenGLHelper::createStandardPipeline(
+                                     const std::string                vertFilePath, ///<
+                                     const std::string                fragFilePath, ///<
+                                     const T                         *pData,        ///<
+                                     const size_t                     numElements,  ///<
+                                     const GLsizei                    totalStride,  ///<
+                                     const std::vector< VAOElement > &elements,     ///<
+                                     const GLenum                     type,         ///<
+                                     const GLenum                     usage         ///<
+                                     )
+{
+  StandardPipeline glIds;
+
+  glIds.program = OpenGLHelper::createProgram(
+                                              vertFilePath,
+                                              fragFilePath
+                                              );
+
+  glIds.vbo = OpenGLHelper::createBuffer(
+                                         pData,
+                                         numElements,
+                                         type,
+                                         usage
+                                         );
+
+  glIds.vao = OpenGLHelper::createVao(
+                                      glIds.program,
+                                      glIds.vbo,
+                                      totalStride,
+                                      elements
+                                      );
+
+  return glIds;
+} // OpenGLHelper::createStandardPipeline
 
 
 
